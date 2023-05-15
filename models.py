3df723cc93bfa0a147410b8f39bda3a3c28478ff
Dreamwave7 <dima190179@gmail.com>
2023-05-15 10:38:25 +0300

    n

---
+++ models.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+from sqlalchemy import Column,Integer, String,Boolean
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.sql.schema import ForeignKey, Table
+from sqlalchemy.sql.sqltypes import DateTime
+
