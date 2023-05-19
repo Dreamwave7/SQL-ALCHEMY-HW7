192682e8cf0e3ba1b9eb6176cfaeb22519cfcaad
Dreamwave7 <dima190179@gmail.com>
2023-05-19 15:23:14 +0300

    d

--- my_select_2.py
+++ my_select_2.py
@@ -1,3 +1,6 @@
 from models import *
 from pprint import pprint
 from sqlalchemy import func,desc
+
+x = session.query(Students.name, func.round(func.avg(Grades.grade))).label("average_grade")\
+    .select_from()
